734ebde7a399a26c66f03453e0cec94237c487a5
Italo Palazzese <italopalazzese96@gmail.com>
2017-05-29 18:54:34 -0600

    Prueba

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/mensaje', function(req,res,next){
+  res.render('prueba',{"txtMensaje":"","msg":""});
+});
+
+var usersRegistered = [];
+
+router.post('/mensaje',function(req,res,next){
+  console.log(req.body);
+    usersRegistered.push(req.body.txtEmail);
+    // Para que conozcan map funcion de un arreglo
+    var msgs = usersRegistered;
+    var rtObject = {}; // {"txtEmail": req.body.txtEmail, "msg":msgs};
+    rtObject.txtEmail = req.body.txtEmail;
+    rtObject.msg= msgs;
+  res.render('register', rtObject);
+});
+
+router.get('/api/usuarios', function(req,res,next){
+  var usuarios = [
+    {"nombre":"Italo"},
+    {"apellido":"Palazzese",},
+    {"cuenta":0801199620983,},
+    {"telefono":32525553}
+  ]
+  res.json(usuarios);
+
+});
+
 module.exports = router;
